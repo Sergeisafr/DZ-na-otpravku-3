4502b6447d69fbbe99acd5d5d8b9a4e38763e7f2
Sergei <Sergeisafr@mail.ru>
2022-08-29 21:02:28 +0500

    Решение задачи 19 палиндром

---
+++ Задача_19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num % 10; // показывает пятую цифру 
+int b = (num / 10) % 10; // показывает четвертую цифру
+int c = (num / 1000) % 10; // показывает вторую цифру
+int d = (num / 10000) % 10; // показывает первую цифру
+if (a == d && c == b)
+{Console.WriteLine( (num) + ", является палиндром");}
+else
+{Console.WriteLine( (num) + ", не является палиндромом");}
