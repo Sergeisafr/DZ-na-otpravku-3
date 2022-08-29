f2bfafd8d39e6b089fbcd38e0f1379d49ff8d351
Sergei <Sergeisafr@mail.ru>
2022-08-29 21:12:46 +0500

    Решение задачи 23 таблица кубов

---
+++ Задача_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int cub = 0;
+while (count <= num) 
+{
+    cub  = Convert.ToInt32(Math.Pow(count, 3)); 
+    count = count + 1;
+    Console.WriteLine(cub);
+}
